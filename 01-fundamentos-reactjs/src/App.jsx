b14c57a6b9b40c97a2bdaf7bc3f91a9b5dc09e59
marinh0w <gabrielgambardella14@gmail.com>
2022-12-20 18:58:39 -0300

    Sidebard 1/2

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,19 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./global.css";
+import styles from "./App.module.css";
 
 export function App() {
   return (
     <div>
-      <Post />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post />
+        </main>
+      </div>
     </div>
   );
 }
