e1b4172e9441dcf79c58b8f82699928643dcb10e
marinh0w <gabrielgambardella14@gmail.com>
2022-12-27 14:34:20 -0300

    phosphor

---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,35 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/marinh0w.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Seu Zé</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="27 de Dezembro às 14:07h" dateTime="2002-27-12 14:07:50">
+          Publicado há
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Por conseguinte, a determinação clara de objetivos cumpre um papel</p>
+        <p>essencial na implantação do sistema de monitoramento corporativo.</p>
+        <p>Considerando que temos bons administradores de rede, a implementação do</p>
+        <p>código apresenta tendências no sentido de aprovar a nova topologia dos</p>
+        <p>paradigmas de desenvolvimento de software. O que temos que ter sempre em</p>
+        <p>mente é que a <a href=""> lógica proposicional </a>faz parte de um processo de</p>
+        <p>gerenciamento de memória avançado da confidencialidade imposta pelo</p>
+        <p>sistema de senhas. Do mesmo modo, a lei de Moore auxilia no aumento da</p>
+      </div>
+    </article>
+  );
+}
--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import {PencilLine} from 'phosphor-react'
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -9,12 +11,16 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/marinh0w.png" />
+
         <strong>Seu Zé</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu Perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu Perfil</a>
       </footer>
     </aside>
   );
