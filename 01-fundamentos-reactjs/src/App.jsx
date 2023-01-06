49bfb47dd967b6f44e7862b260599993febd39ab
marinh0w <gabrielgambardella14@gmail.com>
2023-01-06 17:37:42 -0300

    Propriedades do Post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -5,7 +5,87 @@ import { Sidebar } from "./components/Sidebar";
 import styles from "./App.module.css";
 
 import "./global.css";
-import { Comment } from "./components/Comment";
+
+// author: { avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/marinh0w.png",
+      name: "Gabriel Marinho",
+      role: "Desenvolvedor @ CEL",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content:
+          "Por conseguinte, a determinação clara de objetivos cumpre um papel",
+      },
+      {
+        type: "paragraph",
+        content:
+          "essencial na implantação do sistema de monitoramento corporativo.",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Considerando que temos bons administradores de rede, a implementação do",
+      },
+      {
+        type: "paragraph",
+        content:
+          "código apresenta tendências no sentido de aprovar a nova topologia dos",
+      },
+      { type: "link", content: "paradigmas de desenvolvimento de software." },
+      {
+        type: "paragraph",
+        content:
+          "O que temos que ter sempre em sistema de senhas. Do mesmo modo, a lei de Moore auxilia no aumento da",
+      },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/marinh0w.png",
+      name: "Seu Zé",
+      role: "Desenvolvedor @ Liceu",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content:
+          "Por conseguinte, a determinação clara de objetivos cumpre um papel",
+      },
+      {
+        type: "paragraph",
+        content:
+          "essencial na implantação do sistema de monitoramento corporativo.",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Considerando que temos bons administradores de rede, a implementação do",
+      },
+      {
+        type: "paragraph",
+        content:
+          "código apresenta tendências no sentido de aprovar a nova topologia dos",
+      },
+      { type: "link", content: "paradigmas de desenvolvimento de software." },
+      {
+        type: "paragraph",
+        content:
+          "O que temos que ter sempre em sistema de senhas. Do mesmo modo, a lei de Moore auxilia no aumento da",
+      },
+    ],
+    publishedAt: new Date("2022-01-03 20:00:00"),
+  },
+];
 
 export function App() {
   return (
@@ -15,8 +95,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,50 +1,45 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+
+
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+    
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/marinh0w.png" />
+          <Avatar src={author.avatarUrl}/>
           <div className={styles.authorInfo}>
-            <strong>Seu Zé</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="27 de Dezembro às 14:07h" dateTime="2002-27-12 14:07:50">
-          Publicado há
+        <time title={publishedDateFormatted} dateTime={publishedAt.toIsoString}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>
-          Por conseguinte, a determinação clara de objetivos cumpre um papel
-        </p>
-        <p>essencial na implantação do sistema de monitoramento corporativo.</p>
-        <p>
-          Considerando que temos bons administradores de rede, a implementação
-          do
-        </p>
-        <p>
-          código apresenta tendências no sentido de aprovar a nova topologia dos
-        </p>
-        <p>
-          paradigmas de desenvolvimento de software. O que temos que ter sempre
-          em
-        </p>
-        <p>
-          mente é que a <a href=""> lógica proposicional</a> faz parte de um
-          processo de
-        </p>
-        <p>
-          gerenciamento de memória avançado da confidencialidade imposta pelo
-        </p>
-        <p>
-          sistema de senhas. Do mesmo modo, a lei de Moore auxilia no aumento da
-        </p>
+        {content.map(line => {
+            if (line.type === 'paragraph') {
+                return <p>{line.content}</p>;
+            } else if (line.type === 'link') {
+                return <p><a href="">{line.content}</a></p>;
+            }
+        })}
       </div>
 
       <form className={styles.commentForm}>
